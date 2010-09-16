bd3dea8748a81ff62a0e5d352774b07f11c80c0a
krishna.trial <krishna.trial@04cecf5c-2b72-11df-96cb-b35f494e5c8e>
2010-09-16 17:54:00 +0000

    Some code separation and Tandem related code addition

--- ProteinPipelineScripts/src/org/liverpool/pipeline/ConstructSearchCommand.java
+++ ProteinPipelineScripts/src/org/liverpool/pipeline/ConstructSearchCommand.java
@@ -1,6 +1,8 @@
 package org.liverpool.pipeline;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Set;
@@ -8,6 +10,7 @@ import java.util.Set;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import org.liverpool.utils.ReadConfigurationFiles;
+import org.liverpool.utils.ResolveOmssaModificationIdentifiers;
 import org.liverpool.utils.ValidateInputFiles;
 
 public class ConstructSearchCommand {
@@ -22,15 +25,43 @@ public class ConstructSearchCommand {
 	File allowedKeywordFile;
 	String inputFileDelimiter;
 	
-	ConstructSearchCommand(File input,File template,File allowedKeyword,String inputDelimiter)
-	{
+	File umodFile; 
+	String umodFileDelimiter;	
+	String omssaIdentifierInHeaderInUmodFile;	
+	File enzymeFile; 
+	String enzymeFileDelimiter;
+
+	
+	/**
+	 * 
+	 * @param input
+	 * @param template
+	 * @param allowedKeyword
+	 * @param inputDelimiter
+	 * @param umodFile
+	 * @param umodFileDelimiter
+	 * @param omssaIdentifierInHeaderInUmodFile
+	 * @param enzymeFile
+	 * @param enzymeFileDelimiter
+	 */
+	public ConstructSearchCommand(File input,File template,File allowedKeyword,String inputDelimiter,
+								File umodFile, String umodFileDelimiter, 
+								String omssaIdentifierInHeaderInUmodFile,
+								File enzymeFile, String enzymeFileDelimiter) {
+		
 		rc = new ReadConfigurationFiles();
 		vif = new ValidateInputFiles();
 		 
-		inputFile = input;
-		templateFile = template;
-		allowedKeywordFile = allowedKeyword;
-		inputFileDelimiter = inputDelimiter;
+		this.inputFile = input;
+		this.templateFile = template;
+		this.allowedKeywordFile = allowedKeyword;
+		this.inputFileDelimiter = inputDelimiter;
+		
+		this.umodFile = umodFile;
+		this.umodFileDelimiter = umodFileDelimiter;
+		this.omssaIdentifierInHeaderInUmodFile = omssaIdentifierInHeaderInUmodFile;
+		this.enzymeFile = enzymeFile;
+		this.enzymeFileDelimiter = enzymeFileDelimiter;
 	}
 	
 	
@@ -94,7 +125,7 @@ public class ConstructSearchCommand {
 	 * 
 	 * @return
 	 */
-	public String accpetInputAndCreateCommand() {
+	public String accpetInputAndCreateCommand(String searchEngineName) {
 
 		String command = new String();
 		try{
@@ -103,7 +134,11 @@ public class ConstructSearchCommand {
 			String templateCommand = readTemplateCommandFile();
 			
 			if(vif.validateContentOfInputFileAgainstAllowedKeywords(allowedKeywords, inputHash.keySet())){
-				command = fillTheCommandTemplate(inputHash,templateCommand);
+				if(searchEngineName.equals(Constants.OMSSA_ID))
+					command = fillTheCommandTemplate(inputHash,templateCommand);
+				if(searchEngineName.equals(Constants.TANDEM_ID)){
+					command = fillTheCommandTemplateAfterResolvingForTandem(inputHash,templateCommand);
+				}
 			}
 			else{
 				log.error("Invalid input keywords found in the input file");
@@ -122,6 +157,126 @@ public class ConstructSearchCommand {
 		return command;
 	}
 	
+	
+	/**
+	 * Special processing is required for X!Tandem
+	 * 
+	 * TODO - We need to create Taxonomy file + need to fill the inputHash with the file location
+	 *  
+	 * @return
+	 */
+	String fillTheCommandTemplateAfterResolvingForTandem(HashMap <String, String> inputHash, String templateCommand){
+		
+		String command = new String(templateCommand);
+		
+		try{
+			// Remove all -te, -to etc strings from the input content
+			Iterator<String> keys = inputHash.keySet().iterator();
+			while(keys.hasNext()){
+				String key = keys.next();
+				String [] value = inputHash.get(key).split("\\s"); // split the value for space
+				if(value.length > 1)
+					inputHash.put(key, value[1]);
+			}	
+			
+			// Resolve the mode identifiers
+			ResolveOmssaModificationIdentifiers rom = new ResolveOmssaModificationIdentifiers(this.inputFile, this.inputFileDelimiter,
+												  this.umodFile,this.umodFileDelimiter,
+												  this.omssaIdentifierInHeaderInUmodFile);
+			int [][]modArray = rom.omssaModificationsFromSearchInput();
+			String lineDelimiter = "\n";
+			String modificationString = rom.resolveOmssaModificationNumbers(modArray,lineDelimiter);
+			// TODO create xml string here and put in the hash for fixed, variable mod keys
+			// - WHAT IS THE XML FOR X!TANDEM
+			// Construct xml snippet for  - {{ fixed_mod_id }} 
+			// Construct xml snippet for  - {{ variable_mod_id }}
+			
+			
+			// Resolve all other identifier
+			String enzyme = null;
+			String missedCleavages = null;
+			String product_tol = null;
+			String precursor_tol = null;
+			
+			HashMap <String, String> enzymeFileContent = rc.readInputCsvFile(this.enzymeFile, this.enzymeFileDelimiter);
+			keys = null;
+			keys = inputHash.keySet().iterator();
+			while(keys.hasNext()){
+				String key = keys.next(); 
+				if(key.contains(Constants.SUBSTRING_TO_IDENTIFY_ENZYME)){
+						enzyme = enzymeFileContent.get(inputHash.get(key));
+						//TODO create xml string here and put in the hash - WHAT IS THE XML FOR X!TANDEM
+						// Construct xml snippet for  - {{ enzyme_name }}
+						
+				}
+				if(key.contains(Constants.SUBSTRING_TO_IDENTIFY_MISSED_CLEAVAGES)){
+					missedCleavages = inputHash.get(key);
+					String xmlForMissedCleavages = "<note type = \"input\" label = \"scoring, maximum missed cleavages sites\">" + missedCleavages + "</note>";
+					inputHash.put(key, xmlForMissedCleavages);
+				}
+				if(key.contains(Constants.SUBSTRING_TO_IDENTIFY_PRODUCT)){
+					product_tol = inputHash.get(key);
+					String xmlForFragmentMass  = "<note type=\"heading\">Spectrum general</note> \n" 
+												 + "\t<note type = \"input\" label = \"spectrum, fragment monoisotopic mass error\">" + product_tol + "</note> \n" 
+												 + "\t<note type=\"input\" label=\"spectrum, fragment monoisotopic mass error units\">Da</note>" ;
+					inputHash.put(key, xmlForFragmentMass);
+				}
+				if(key.contains(Constants.SUBSTRING_TO_IDENTIFY_PRECURSOR)){
+					precursor_tol = inputHash.get(key);
+					String xmlForParenttMass = "<note type=\"input\" label=\"spectrum, parent monoisotopic mass error plus\">"  + precursor_tol  + "</note> \n"
+											  + "\t<note type=\"input\" label=\"spectrum, parent monoisotopic mass error minus\">"+ precursor_tol + "</note> \n"
+											  + "\t<note type=\"input\" label=\"spectrum, parent monoisotopic mass error units\">ppm</note> \n"
+											  + "\t<note type=\"input\" label=\"spectrum, parent monoisotopic mass isotope error\">yes</note>";
+					inputHash.put(key, xmlForParenttMass);
+				}
+				
+				// Check if the taxonomy file exists, if not, then create a new one on that location
+				if(key.contains(Constants.SUBSTRING_TO_IDENTIFY_TAXONOMY_FILE)){
+					try{
+							File taxonomyFile = new File(inputHash.get(key));
+							if(taxonomyFile.exists())
+								taxonomyFile.delete();
+							taxonomyFile.createNewFile();
+							String taxonomyTemplate = "<?xml version=\"1.0\"?> \n" +
+													  "<bioml label=\"x! taxon-to-file matching list\"> \n" +
+													  "<taxon label = \"{{ " + Constants.STRING_TO_IDENTIFY_SPECIES +" }}\">" +
+													  "<file format=\"peptide\" URL=\"{{ "+ Constants.STRING_TO_IDENTIFY_FASTA_FILE +" }}\" />" +
+													  "</taxon>" +
+													  "</bioml>";
+							String taxonomyContent = fillTheCommandTemplate(inputHash,taxonomyTemplate);
+							taxonomyContent = removeExtraPalceholdersFromTemplate(taxonomyContent);
+							BufferedWriter br = new BufferedWriter(new FileWriter(taxonomyFile));
+							br.write(taxonomyContent);
+							br.close();
+							
+							log.info("Taxonomy file content : \n" + taxonomyContent);
+							
+						}catch(Exception e){
+							log.fatal("Problem creating Taxonomy file");
+							e.printStackTrace();
+						}
+				}
+			}
+		
+
+			// After resolving all the references, fill the template.
+			Iterator <String> inputkeys = inputHash.keySet().iterator();
+			while(inputkeys.hasNext()){
+				String key = inputkeys.next();
+				String textToReplace = "{{ " + key.trim() + " }}";
+				command = command.replace(textToReplace,inputHash.get(key));
+			}
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		
+		return command;
+	}
+	
+	
+	
 	/**
 	 * 
 	 * @param args
@@ -134,19 +289,33 @@ public class ConstructSearchCommand {
 		File template = new File("templates/omssa_template.txt");
 		File allowedKeyword = new File("resources/omssaKeywords.txt");
 		String inputDelimiter = "=";
+		String searchEngineName = Constants.OMSSA_ID;
 		
-		ConstructSearchCommand cs = new ConstructSearchCommand(input,template,allowedKeyword,inputDelimiter);
-		String command = cs.accpetInputAndCreateCommand();
+		File umodFile								= new File("resources/UMOD_TABLE.csv"); 
+		String umodFileDelimiter					= ",";	
+		String omssaIdentifierInHeaderInUmodFile	= "Omssa_ID";	
+		File enzymeFile							    = new File("resources/enzymeList.csv"); 
+		String enzymeFileDelimiter					= "=";
+
+		
+		ConstructSearchCommand cs = new ConstructSearchCommand(input,template,allowedKeyword,inputDelimiter,
+																umodFile, umodFileDelimiter, 
+																omssaIdentifierInHeaderInUmodFile,
+																enzymeFile, enzymeFileDelimiter);
+				
+		String command = cs.accpetInputAndCreateCommand(searchEngineName);
 		
 		// For X!Tandem
 		log.info("Constructing command for X!Tandem...");
-		input = new File("inputFiles/tandem_inputFile.txt");
 		template = new File("templates/tandem_template.txt");
-		allowedKeyword = new File("resources/tandemKeywords.txt");
-		inputDelimiter = "=";
+		searchEngineName = Constants.TANDEM_ID;
 		
-		cs = new ConstructSearchCommand(input,template,allowedKeyword,inputDelimiter);
-		command = cs.accpetInputAndCreateCommand();
+		cs = new ConstructSearchCommand(input,template,allowedKeyword,inputDelimiter,
+										umodFile, umodFileDelimiter, 
+										omssaIdentifierInHeaderInUmodFile,
+										enzymeFile, enzymeFileDelimiter);
+
+		command = cs.accpetInputAndCreateCommand(searchEngineName);
 		
 		
 	}
